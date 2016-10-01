7057f176f52da18fc8de0484279ba689ff19a4f0
= <=>
2016-10-01 14:55:24 +0200

    Added fields to User object

--- RiderPOC/User.cs
+++ RiderPOC/User.cs
@@ -4,6 +4,10 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public int ZipCode { get; set; }
+        public string City { get; set; }
     }
 
     public class ObjectBase
