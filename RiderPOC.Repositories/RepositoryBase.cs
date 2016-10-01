24508428209c51ea8162ea6648ec69afeb04bee6
= <=>
2016-10-01 18:25:45 +0200

    Added MongoDB driver

---
+++ RiderPOC.Repositories/RepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace RiderPOC.Repositories
+{
+    public class RepositoryBase
+    {
+        public bool DoHeartBeat(string databaseName)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase(databaseName);
+            return database != null;
+        }
+    }
+}
+
+
--- RiderPOC.Tests/UserServiceTests.cs
+++ RiderPOC.Tests/UserServiceTests.cs
@@ -1,5 +1,7 @@
-﻿using NFluent;
+﻿using System.Data;
+using NFluent;
 using NUnit.Framework;
+using RiderPOC.Repositories;
 
 namespace RiderPOC.Tests
 {
@@ -11,6 +13,14 @@ namespace RiderPOC.Tests
             var userSerice = new UserService();
             var returnValue = userSerice.GetUserById(2);
             Check.That(returnValue.FirstName).Equals("Robin");
+        }
+
+        [TestCase("default", true)]
+        [TestCase("should'nt fucking exist", false)]
+        public void Should_Be_Able_To_Connect_To_MongoDB(string databaseName, bool expected)
+        {
+            var repo = new RepositoryBase();
+            Check.That(repo.DoHeartBeat(databaseName)).Equals(expected);
         }
     }
 }
